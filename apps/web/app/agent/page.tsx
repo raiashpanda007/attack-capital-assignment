027048920be7739752f598cfd3a9309f867aadd3
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-09-18 17:51:36 +0530

    fix: adjust button layout for improved alignment in agent page

--- apps/web/app/agent/page.tsx
+++ apps/web/app/agent/page.tsx
@@ -72,7 +72,7 @@ export default function Page() {
     <form className="h-screen w-full text-center flex flex-col justify-evenly" onSubmit={joinCall}>
       <h1 className="text-5xl font-semibold"> Welcome to RelayCall an Attack Capital assignment project</h1>
 
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/4 self-center mt-10 cursor-pointer" onClick={joinCall}>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/4 flex self-center mt-10 cursor-pointer items-center justify-center" onClick={joinCall}>
         <MdCall className="mx-3" /> Join as Agent
       </button>
 
